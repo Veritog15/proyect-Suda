fd35c2c0a1c807a49403cbede85783cd0ddbcd4e
Estre05 <165815792+Estre05@users.noreply.github.com>
2024-08-07 16:11:42 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TextComponent } from "../components/text/text.component";
+import { ButtonComponent } from '../components/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, TextComponent],
+  imports: [RouterOutlet, NavbarComponent, TextComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TextComponent } from '../../components/text/text.component';
+import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [TextComponent],
+  imports: [TextComponent, ButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })

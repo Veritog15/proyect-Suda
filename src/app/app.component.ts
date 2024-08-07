7aaaee40b3c48fe5ab455b9d1cb6e8537f680465
Estre05 <165815792+Estre05@users.noreply.github.com>
2024-08-07 15:04:40 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TextComponent } from "../components/text/text.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent ],
+  imports: [RouterOutlet, NavbarComponent, TextComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TextComponent } from '../../components/text/text.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [TextComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })

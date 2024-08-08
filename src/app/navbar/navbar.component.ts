d3cf5cf0a2f87c6677954068f2837f31150d7884
Estre05 <165815792+Estre05@users.noreply.github.com>
2024-08-08 00:09:11 -0500

    commit

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TextComponent } from '../../components/text/text.component';
 import { ButtonComponent } from "../../components/button/button.component";
+import { CardsComponent } from '../../components/cards/cards.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [TextComponent, ButtonComponent],
+  imports: [TextComponent, ButtonComponent, CardsComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })

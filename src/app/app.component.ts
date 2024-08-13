199767f4becf3de9c2f28e55948820d3dc7aba1c
Estre05 <165815792+Estre05@users.noreply.github.com>
2024-08-13 14:21:04 -0500

    COMMIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { ScrollComponent } from '../components/scroll/scroll.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent,ScrollComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

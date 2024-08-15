281f1842307b2f3b2f81154c9447330177bc9288
Estre05 <165815792+Estre05@users.noreply.github.com>
2024-08-15 00:04:12 -0500

    commit

--- src/app/pages/thematic/thematic.component.ts
+++ src/app/pages/thematic/thematic.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeadComponent } from '../../../components/head/head.component';
 
 @Component({
   selector: 'app-thematic',
   standalone: true,
-  imports: [],
+  imports: [HeadComponent],
   templateUrl: './thematic.component.html',
   styleUrl: './thematic.component.css'
 })

3c0314003bd74cf18cd21b68c2ae3c8db56d7d71
Estre05 <165815792+Estre05@users.noreply.github.com>
2024-08-15 22:31:48 -0500

    commit

--- src/app/pages/information/information.component.ts
+++ src/app/pages/information/information.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeadComponent } from '../../../components/head/head.component';
 import { Button2Component } from "../../../components/button2/button2.component";
+import { ButtonComponent } from '../../../components/button/button.component';
+import { FooterComponent } from "../../../components/footer/footer.component";
 
 @Component({
   selector: 'app-information',
   standalone: true,
-  imports: [HeadComponent, Button2Component],
+  imports: [HeadComponent, ButtonComponent, Button2Component, FooterComponent],
   templateUrl: './information.component.html',
   styleUrl: './information.component.css'
 })
